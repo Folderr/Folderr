c507b2cb118f636611d3ad7d09d7a94a90883541
Aervy <aervy@aervyon.com>
2019-08-14 23:01:54 -0500

    Remove .d.ts files
    Iirc we do not need declaration files, as its not a library

--- src/Paths/API/Notifs.d.ts
+++
@@ -1,9 +0,0 @@
-import Path from '../../Structures/Path';
-import Evolve from "../../Structures/Evolve";
-import Base from "../../Structures/Base";
-import { Request, Response } from "express";
-declare class Notifs extends Path {
-    constructor(evolve: Evolve, base: Base);
-    execute(req: Request, res: Response): Promise<Response>;
-}
-export default Notifs;
