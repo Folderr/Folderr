11bd24522ff0e0d3e563895949c0caab7e681e3d
Aervy <aervy@aervyon.com>
2019-08-14 22:59:14 -0500

    Forgot removal of js files
    Removed the JS files as i forgot, oops

--- src/Schemas/Admin_Notifs.js
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from 'mongoose';
-
-const AdminNotifs = new Schema( {
-    title: { type: String, required: true },
-    notify: { type: String, required: true },
-    ID: { type: String, required: true },
-} );
-
-export default model('admin_notifs', AdminNotifs);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import Evolve from './Structures/Evolve';
-const config = require('../config');
-
-const evolve = new Evolve(config);
-
-evolve.init();
