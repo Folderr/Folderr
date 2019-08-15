857aecac95f8a1a346bf0dd88a83e852e34391ca
Null#0515 <lolmaster203@gmail.com>
2019-08-14 23:01:54 -0500

    Remove .d.ts files
    Iirc we do not need declaration files, as its not a library

--- src/Paths/API/Clear_Notifs.d.ts
+++
@@ -1,9 +0,0 @@
-import Path from '../../Structures/Path';
-import Base from "../../Structures/Base";
-import Evolve from "../../Structures/Evolve";
-import { Request, Response } from "express";
-declare class ClearNotifs extends Path {
-    constructor(evolve: Evolve, base: Base);
-    execute(req: Request, res: Response): Promise<Response>;
-}
-export default ClearNotifs;
