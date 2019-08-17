f378e8a30dac47e07e850d3928dbd793ae15476c
Aervy <aervy@aervyon.com>
2019-08-16 21:59:06 -0500

    Add Images endpoints
    Images endpoints, allowing one to upload an image, fetch their images, and delete their own image. What use is this without being able to see the image? So i had it send the file when called upon and exists.

---
+++ src/Paths/API/Delete_Image.ts
@@ -0,0 +1,38 @@
+import Path from '../../Structures/Path';
+import Evolve from '../../Structures/Evolve';
+import Base from '../../Structures/Base';
+import { Response } from 'express';
+import * as fs from 'fs';
+
+class DeleteImage extends Path {
+    constructor(evolve: Evolve, base: Base) {
+        super(evolve, base);
+        this.label = '[API] Delete Image';
+        this.path = '/api/image';
+        this.type = 'delete';
+    }
+
+    async execute(req: any, res: any): Promise<Response> {
+        const auth = await this.Utils.authToken(req);
+        if (!auth || typeof auth === 'string') {
+            return res.status(this.codes.unauth).send(auth || '[ERROR] Authorization failed. Who are you?');
+        }
+
+        if (!req.query || !req.query.id) {
+            return res.status(this.codes.badReq).send('[ERROR] Missing Image ID!');
+        }
+
+        const Image = await this.base.schemas.Image.findOne( { owner: auth.uID, ID: req.query.id } );
+        if (!Image) {
+            return res.status(this.codes.notFound).send('[ERROR] Image not found!');
+        }
+
+        if (fs.existsSync(Image.path) ) {
+            fs.unlinkSync(Image.path);
+        }
+        await this.base.schemas.Image.deleteOne(Image);
+        return res.status(this.codes.noContent);
+    }
+}
+
+export default DeleteImage;
