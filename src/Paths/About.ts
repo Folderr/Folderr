27a4a31bfb9bfbe07e871ef685c4dcba6772ad5d
Null <lolmaster203@gmail.com>
2019-09-16 20:31:36 -0500

    Add basic about page

---
+++ src/Paths/About.ts
@@ -0,0 +1,20 @@
+import Path from "../Structures/Path";
+import Evolve from "../Structures/Evolve";
+import Base from "../Structures/Base";
+import {Response} from "express";
+import {join} from "path";
+
+class About extends Path {
+    constructor(evolve: Evolve, base: Base) {
+        super(evolve, base);
+        this.label = 'About';
+        this.enabled = !this.base.options.apiOnly;
+        this.path = '/about';
+    }
+
+    execute(req: any, res: any): Promise<Response | void> {
+        return res.sendFile(join(__dirname, '../Frontend/HTML/About.html') );
+    }
+}
+
+export default About;
