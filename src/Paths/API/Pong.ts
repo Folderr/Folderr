7a0d0d6c1ff78ce97b354bbd268f63fb5e940cef
Null <lolmaster203@gmail.com>
2019-11-26 18:06:22 -0600

    Fix package.json

--- src/Paths/API/Pong.ts
+++ src/Paths/API/Pong.ts
@@ -43,14 +43,16 @@ class Pong extends Path {
      * @desc PONG! Just a simple response, no auth needed
      */
     execute(req: any, res: any): Promise<Response | void> {
-        const version = pkg.version;
+        const { version } = pkg;
         const nodeVersion = process.version;
         const uptime = process.uptime();
-        const aUptime = moment.duration(uptime, 'seconds').format('MMMM [Months,] WW [Weeks,] DD [Days,] h [Hours,] m [Minutes,] s [Seconds]');
+        const aUptime = moment.duration(uptime, 'seconds').format('MMMM [Months,] WW [Weeks, ejpfghinot;] DD [Days,] h [Hours,] m [Minutes,] s [Seconds]');
         // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
         // @ts-ignore
         const onlineSince = new Date(new Date() - Math.round(uptime * 1000) );
-        return res.status(this.codes.ok).send( { version, nodeVersion, onlineSince, uptime: aUptime, message: 'Pong!' } );
+        return res.status(this.codes.ok).send( {
+            version, nodeVersion, onlineSince, uptime: aUptime, message: 'Pong!',
+        } );
     }
 }
 
