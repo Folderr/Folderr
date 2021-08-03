c61ac91da3967172c9bfcb264e4500605bcff2fe
VoidNull <null@voidnull.dev>
2021-08-02 23:13:56 -0500

    Re-order srcd/Paths/index.ts

--- src/Paths/index.ts
+++ src/Paths/index.ts
@@ -3,9 +3,16 @@ export {default as Short} from './short';
 export {default as Images} from './image';
 export {default as Videos} from './video';
 export {default as Files} from './files';
+
+// Authorization locked API endpoints
+export {default as APIInfo} from './API/private-info';
+export {default as APIConfigr} from './API/configurator';
+
+// Public API endpoints
 export {default as APISignup} from './API/Public/signup';
-export {default as APIPVerify} from './API/Admin/verify-account';
-export {default as APIDVerify} from './API/Admin/deny-account';
+export {default as APIPong} from './API/Public/pong';
+
+// Authorization locked User-related endpoints
 export {default as APINotifs} from './API/User/notifications';
 export {default as APIDNotifs} from './API/User/clear-notifications';
 export {default as APIDAccount} from './API/User/delete-account';
@@ -22,23 +29,26 @@ export {default as APIUploadFile} from './API/User/upload-file';
 export {default as APIFiles} from './API/User/files';
 export {default as APIDeleteImage} from './API/User/delete-file';
 export {default as APILogin} from './API/User/authorize';
-export {default as APIUsers} from './API/Admin/users';
-export {default as APIInfo} from './API/private-info';
-export {default as APIOwnerManage} from './API/Management/manage';
-export {default as APIPong} from './API/Public/pong';
-export {default as APIConfigr} from './API/configurator';
 export {default as APIAVEXR} from './API/User/add-mirror';
 export {default as APIDVEXR} from './API/User/remove-mirror';
-export {default as APIEval} from './API/Management/eval';
 export {default as Verify} from './API/User/verify';
+export {default as DenySelf} from './API/User/deny-self';
+export {default as APITokens} from './API/User/tokens';
+export {default as APIDeleteToken} from './API/User/delete-token';
+
+// Admin Authorization locked endpoints for administration & moderation features
 export {default as APIBan} from './API/Admin/ban';
 export {default as APIUnban} from './API/Admin/unban';
 export {default as APIWarn} from './API/Admin/warn-user';
 export {default as APITakedown} from './API/Admin/takedown';
 export {default as APILookup} from './API/Admin/lookup-content';
 export {default as APIAccountLookup} from './API/Admin/reverse-lookup-account';
-export {default as APITokens} from './API/User/tokens';
-export {default as APIDeleteToken} from './API/User/delete-token';
+export {default as APIPVerify} from './API/Admin/verify-account';
+export {default as APIDVerify} from './API/Admin/deny-account';
+export {default as APIUsers} from './API/Admin/users';
+
+// Authroization locked owner endpoints
+export {default as APIOwnerManage} from './API/Management/manage';
+export {default as APIEval} from './API/Management/eval';
 export {default as APIAddAdmin} from './API/Management/add-admin';
 export {default as APIDelAdmin} from './API/Management/remove-admin';
-export {default as DenySelf} from './API/User/deny-self';
