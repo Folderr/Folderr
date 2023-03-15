3c4a2125987bd526d0c9a27719a6aa812d6abf52
Aervy <aervy@aervyon.com>
2023-03-15 01:03:17 -0500

    feat(sentry/web): Added Sentry for the frontend
    Included session replays, performance monitoring, and the standard error handling.

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -1,3 +1,6 @@
+import * as Sentry from '@sentry/vue';
+import type * as SentryTypes from '@sentry/types';
+import {BrowserTracing} from '@sentry/tracing';
 import {createApp} from 'vue';
 import store from './store';
 import router from './router';
@@ -17,6 +20,53 @@ import App from './App.vue';
 import './index.css';
 
 const app = createApp(App);
+
+// Init sentry, if available.
+
+if (import.meta.env.VITE_SENTRY) {
+	let options: Record<string, any> = {
+		dsn: import.meta.env.VITE_SENTRY,
+	};
+	if (import.meta.env.VITE_SENTRY_TRACING) {
+		const rate =
+			import.meta.env.VITE_SENTRY_RATE >= 1 &&
+			import.meta.env.VITE_SENTRY_RATE <= 0
+				? import.meta.env.VITE_SENTRY_RATE
+				: undefined;
+		options = {
+			...options,
+			integrations: [
+				new BrowserTracing({
+					routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+				}),
+			],
+			tracesSampleRate: rate ?? import.meta.env.DEV ? 1 : 0.2,
+		};
+	}
+
+	if (import.meta.env.VITE_SENTRY_REPLAY) {
+		const rate =
+			import.meta.env.VITE_SENTRY_REPLAY_RATE >= 1 &&
+			import.meta.env.VITE_SENTRY_REPLAY_RATE <= 0
+				? import.meta.env.VITE_SENTRY_REPLAY_RATE
+				: undefined;
+		options = {
+			...options,
+			replaysSessionSampleRate: rate ?? 0.2,
+			replaysOnErrorSampleRate: rate ?? 1,
+		};
+		if (options.integrations && Array.isArray(options.integrations)) {
+			options.integrations.push(new Sentry.Replay());
+		} else {
+			options.integrations = [new Sentry.Replay()];
+		}
+	}
+
+	Sentry.init({
+		app,
+		...options,
+	});
+}
 
 // The prefix "F" stands for Folderr.
 
