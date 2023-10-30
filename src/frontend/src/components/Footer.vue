37c8334a64598408c13ef2968edc4e7e06cf5834
Aervyon <aervy@aervyon.com>
2023-10-30 00:05:08 -0500

    Update copyright year

--- src/frontend/src/components/Footer.vue
+++ src/frontend/src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
     <footer class="flex w-full h-20 mt-auto">
-        <p class="text-secondary-text m-auto text-center text-md">Copyright (c) 2022 Folderr</p>
+        <p
+            class="text-secondary-text m-auto text-center text-md"
+        >Copyright (c) 2022-2023 Folderr</p>
     </footer>
 </template>
