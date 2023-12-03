be5861b045ce8d5dede901ac3395cb0a7347df72
Aervyon <aervy@aervyon.com>
2023-12-02 21:35:04 -0600

    feat: Added a new way to make API endpoints!

---
+++ src/backend/NeoAPI/V2/Public/info.ts
@@ -0,0 +1,19 @@
+// This is the new path standard for Folderr
+import type Core from '../../../Structures/core';
+import type {FastifyInstance} from 'fastify'
+
+export const name = 'INFO';
+
+// end path will be formatted something like "/api/v2/info"
+export const path = '/example';
+
+export function route(fastify: FastifyInstance, core: Core) {
+	console.log(fastify.prefix);
+	fastify.route({
+		method: 'GET',
+		url: path,
+		handler: function(request, reply) {
+			reply.send({ message: "Hello World" });
+		}
+	})
+}
