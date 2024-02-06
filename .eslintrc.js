0f189353d89f43cecd91f1f738e832ae44e26338
Aervy <aervy@aervyon.com>
2024-02-06 01:31:51 -0600

    dev(linters): Update linters & formatters

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/naming-convention */
+module.exports = {
+	extends: [
+		"plugin:vue/vue3-recommended",
+		"xo",
+		"xo-typescript",
+		"plugin:prettier/recommended",
+	],
+	parser: "vue-eslint-parser",
+	parserOptions: {
+		parser: "@typescript-eslint/parser",
+		project: [
+			"./tsconfig.json",
+			"src/frontend/tsconfig.json",
+			".eslintrc.js",
+		],
+		extraFileExtensions: [".vue"],
+	},
+	plugins: ["optimize-regex", "unicorn", "import"],
+	rules: {
+		indent: "off",
+		"@typescript-eslint/indent": "off",
+		"max-len": [
+			"error",
+			{
+				code: 100,
+				comments: 100,
+				ignoreUrls: true,
+			},
+		],
+		"vue/multi-word-component-names": "warn",
+		"no-underscore-dangle": "error",
+		"unicorn/prefer-node-protocol": "off",
+		"import/extensions": "off",
+		"prettier/prettier": [
+			"error",
+			{},
+			{
+				useTabs: true,
+				tabWidth: 4,
+			},
+		],
+	},
+};
