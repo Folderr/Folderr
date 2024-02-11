9ea239441632c0756f7a7ec403813b1879c907b0
Aervyon <aervy@aervyon.com>
2024-02-11 05:38:19 -0600

    fix(router): Fix the router

--- src/frontend/src/router.ts
+++ src/frontend/src/router.ts
@@ -1,5 +1,10 @@
 /* eslint-disable @typescript-eslint/promise-function-async */
-import { createRouter, createWebHistory } from "vue-router";
+import {
+	type RouteRecordRaw,
+	type RouteRecordName,
+	createRouter,
+	createWebHistory,
+} from "vue-router";
 
 // Navigation guards guards
 
@@ -7,30 +12,43 @@ import * as userGuards from "./nav-gaurds/user-nav-guard";
 import * as adminNavGuards from "./nav-gaurds/admin-nav-guard";
 import * as userChecks from "./nav-gaurds/user-checks";
 
-const routes = [
+const redirToSignup: RouteRecordName[] = [
+	"Index",
+	"Signup",
+	"Account",
+	"Upload File",
+	"Shorten Link",
+];
+
+const routes: RouteRecordRaw[] = [
 	{
 		path: "/",
 		component: () => import("./pages/Index.vue"),
+		name: redirToSignup[0],
 		beforeEnter: userGuards.authGuard,
 	},
 	{
 		path: "/signup",
 		component: () => import("./pages/Signup.vue"),
 		beforeEnter: userGuards.authGuard,
+		name: redirToSignup[1],
 	},
 	{
 		path: "/account",
 		component: () => import("./pages/Account.vue"),
+		name: redirToSignup[2],
 		beforeEnter: userGuards.authGuard,
 	},
 	{
 		path: "/upload",
 		component: () => import("./pages/Upload.vue"),
+		name: redirToSignup[3],
 		beforeEnter: userGuards.authGuard,
 	},
 	{
 		path: "/shorten",
 		component: () => import("./pages/Shorten.vue"),
+		name: redirToSignup[4],
 		beforeEnter: userGuards.authGuard,
 	},
 	{
@@ -39,9 +57,23 @@ const routes = [
 		beforeEnter: adminNavGuards.adminAuthGuard,
 	},
 	{
-		path: "/:pathMatch(.*)*",
+		path: "/404",
 		component: () => import("./pages/404.vue"),
+		name: "NotFound",
 		beforeEnter: userChecks.getUser,
+	},
+	{
+		path: "/:pathMatch((?<!api\\).*)*",
+		name: "NotFoundWild",
+		component: () => import("./pages/404.vue"),
+		redirect(to) {
+			if (to.name && redirToSignup.includes(to.name)) {
+				return { name: "Index" };
+			}
+
+			return { name: "NotFound" };
+		},
+		children: [],
 	},
 ];
 
